03aefb25a0a283bc59f7dd73078ce5ff9ba74521
mike vallano <mikevallano@gmail.com>
2015-05-29 16:14:32 -0500

    added then commented out looping

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,20 @@
 class StaticPagesController < ApplicationController
+
+  # def index
+  #   @results = []
+  #   @listomovies = ["pulp fiction", "interstellar", "birdman", "the imitation game"]
+  #   @listomovies.each do |i|
+  #     movie_title = i.gsub(" ","-")
+  #     # movie_title = params[:movie_title].gsub(" ","-")
+  #     content = open("http://www.omdbapi.com/?t=#{movie_title}&y=&plot=short&r=json").read
+  #     @results << JSON.parse(content, symbolize_names: true)
+  #   end
+  # end
+
   def index
     movie_title = params[:movie_title].gsub(" ","-")
     content = open("http://www.omdbapi.com/?t=#{movie_title}&y=&plot=short&r=json").read
     @results = JSON.parse(content, symbolize_names: true)
   end
+
 end
