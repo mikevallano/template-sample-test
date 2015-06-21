09be8f9d5c21d00243d2e73747fa5a3784498bba
mike vallano <mikevallano@gmail.com>
2015-06-21 15:46:22 -0500

    Refactored JS in static_pages.js for javatester.html.erb

--- app/assets/javascripts/kittens.js
+++ app/assets/javascripts/kittens.js
@@ -2,9 +2,3 @@
 // All this logic will automatically be available in application.js.
 
 
-jQuery(document).ready(function(){
-  console.log("the first kitty is: " + gon.first)
-  gon.kittens.map( function(item) {
-     console.log("this kitty's name is: " + item.name);
-})
-});
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function() {
 
-  // $('#main').hide();
+  $('#main').hide();
 
   var tacos = "i like submitting tacos";
   console.log(tacos);
@@ -13,13 +13,21 @@ $(document).ready(function() {
 //   $('#header').css("background-color","yellow")
 //   });
 
-$( "#header" ).hover(
-  function() {
-    $( this ).css("background-color","yellow");
+$("#header").hover(function() {
+    $(this).css("background-color","yellow");
   }, function() {
-    $( this ).css("background-color","white");
+    $(this).css("background-color","white");
   }
 );
+
+ $(function(){
+    var ogtext = $('#header').text();
+     $('#header').click(function () {
+        $(this).text(function(i, text){
+            return text === ogtext ? "i done changed" : ogtext;
+        })
+     });
+  })
 
 $('#loner_taco').hide();
 $('#loner_taco').val(tacos);
@@ -27,63 +35,26 @@ $('#loner_taco').val(tacos);
 
 
 $('#main').hide();
-
-  $('#kitten_name').hide();
-  $('#kittyfield').hide();
-  $('#kitfield').hide();
-  $('#softnessfield').hide();
-  $('#softnessid').hide();
-  $('#new_kitty_button').hide();//Initially form wil be hidden.
-  $('#kittybutton').click(function() {
-    $('#kitten_name').slideDown();
-    $('#kitfield').slideDown();
-    $('#kittyfield').slideDown();
-    $('#softnessfield').slideDown();
-    $('#softnessid').slideDown();
-    $('#new_kitty_button').slideDown();
+$('#kittyform').hide();
+$('#kittybutton').on('click', function() {
+    $('#kittyform').slideToggle();
   });
 
 
   // Handle form submission
-    $("#new_kitty_button").click(function(e) {
-      e.preventDefault();
+$("#new_kitty_button").on('click', function(event) {
+  $('#kittyform').slideUp();
+  event.preventDefault();
 
-      $('#kitten_name').slideUp();
-      $('#kittyfield').slideUp();
-      $('#kitfield').slideUp();
-      $('#softnessfield').slideUp();
-      $('#softnessid').slideUp();
-      $('#new_kitty_button').slideUp();
+    // Valid form, proceed
+    $.post("/kittens/", {
+      "kitten[name]": $("#kittyfield").val(),
+      "kitten[softness]": $("#softnessid").val()
+    })
+      .done(function(data) {
+      var newkitname = "new kitty name: " + $("#kittyfield").val();
+      console.log(newkitname);
+  });
+});
 
-        // Valid form, proceed
-        $.post("/kittens/", {
-          "kitten[name]": $("#kittyfield").val(),
-          "kitten[softness]": $("#softnessid").val()
-        })
-          .done(function(data) {
-          var newkitname = "new kitty name: " + $("#kittyfield").val();
-          console.log(newkitname);
-      });
-    });
-
-     });
-
-  // <%= f.collection_select(:category_ids, Category.all, :id, :name, { :label => "Categories", :include_blank => "Information" }, { :multiple => true, :size => 5 } ) %>
-  //  <div class="form-group">
-  //   <label class="control-label col-md-2 required"></label>
-  //     <div class="col-md-8" id="show-add-category-field">
-  //      <%= submit_tag '+', :type => 'button', :class => "btn btn-primary" %>
-  //     </div>
-  //     <div class="form-group">
-  //       <label class="control-label col-md-2 required"></label>
-  //         <div class="col-md-8 col-md-offset-2" id="add-category-field">
-  //           <%= text_field_tag :new_category_name, nil, :placeholder => "Category Name", :class => "form-control" %>
-  //         </div>
-  //         <div class="col-md-8 col-md-offset-2" id="add-category-description" style="display: none;">
-  //           <%= text_area :new_category_description, nil, :placeholder => "Category Description", :class => "form-control" %>
-  //         </div>
-  //     </div>
-  //     <div class="col-md-8 col-md-offset-2" id="submit-new-category">
-  //       <%= button_tag 'Add', :id => "new_category_button",  :type => 'button', :class => "btn btn-primary" %>
-  //     </div>
-  //   </div>
+ });
