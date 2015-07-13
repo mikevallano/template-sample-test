6a6b00d7886ffd1abf56d425b3927b2c515ed30d
mike vallano <mikevallano@gmail.com>
2015-07-13 14:01:51 -0500

    Empty migration test

---
+++ db/migrate/20150708145322_add_empty_migration.rb
@@ -0,0 +1,5 @@
+class AddEmptyMigration < ActiveRecord::Migration
+  def change
+    add_column :events, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622215704) do
+ActiveRecord::Schema.define(version: 20150708145322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20150622215704) do
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.integer  "gatito_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.datetime "starts_at"
     t.datetime "ends_at"
+    t.text     "description"
   end
 
   create_table "kittens", force: :cascade do |t|
