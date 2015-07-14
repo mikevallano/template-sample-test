09fb3ee067d27d59c5bfa2668c13ecf82fe81745
mike vallano <mikevallano@gmail.com>
2015-07-13 20:27:36 -0500

    tweaked javascript

--- app/assets/javascripts/z.js
+++ app/assets/javascripts/z.js
@@ -10,17 +10,19 @@ $("#jaxy").click(function() {
 
 var testli = "<li>I am text. Hear me roar!</li>";
 
-// $("#jaxyinfo").click(function() {
-//     $(this).append(testli);
-//   });
 
-  // $('#jaxyinfo').on("click", function() {
-  //     $.ajax('http://localhost:5000/kittens', {
-  //       success: function(response) {
-  //         $('#jaxyinfo').html(response).fadeIn();
-  //       }
-  //     });
-  // });
+// functioning ajax call:
+// $('#jaxyinfo').on("click", function() {
+//   $.ajax({
+//     type: 'GET',
+//     url: '/kittens.json',
+//     success: function(data) {
+//       $.each(data, function(i, kitteh) {
+//         $('#jaxyinfo').append('<li>' + kitteh.name + '</li>');
+//       })
+//     }
+//   });
+// });
 
 $('#jaxyinfo').on("click", function() {
   $.ajax({
@@ -29,7 +31,23 @@ $('#jaxyinfo').on("click", function() {
     success: function(data) {
       $.each(data, function(i, kitteh) {
         $('#jaxyinfo').append('<li>' + kitteh.name + '</li>');
+        console.log('success is firing');
       })
+    },
+    error: function(request, errorType, errorMessage) {
+      alert('OOPSIE. You got an error of type: ' + errorType + ' with message: ' + errorMessage);
+    },
+    // timeout defines milliseconds until a timeout happens
+    timeout: 3000,
+    // beforeSend fires before success in the time it takes to send the call and receive back an error or success.
+    beforeSend: function() {
+      $('#jaxyinfo').addClass('is-loading');
+      console.log('before send message is firing');
+    },
+    // complete gets called after both success and error
+    complete: function () {
+      $('#jaxyinfo').removeClass('is-loading');
+      console.log('complete message is firing')
     }
   });
 });
